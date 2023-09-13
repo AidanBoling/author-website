f1a962195c69abd6a06ac0df700d954f3219244c
Aidan Boling <aidanboling@gmail.com>
2023-09-13 14:32:51 -0700

    Change navbar responsive menu to drawer component.

--- client/src/main/components/Navbar.jsx
+++ client/src/main/components/Navbar.jsx
@@ -4,11 +4,18 @@ import {
     AppBar,
     Box,
     Toolbar,
+    Drawer,
     Menu,
     MenuItem,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
     Typography,
     Link,
     IconButton,
+    Divider,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import ColorModeButton from './ColorModeButton';
@@ -25,14 +32,18 @@ const pages = [
 const title = 'Jane Austen';
 
 function Navbar() {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [drawerState, setDrawerState] = useState(false);
 
-    function handleOpenNavMenu(event) {
-        setAnchorElNav(event.currentTarget);
-    }
-    function handleCloseNavMenu() {
-        setAnchorElNav(null);
-    }
+    const toggleDrawer = open => event => {
+        if (
+            event.type === 'keydown' &&
+            (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+            return;
+        }
+
+        setDrawerState(open);
+    };
 
     return (
         <AppBar position="static">
@@ -52,7 +63,7 @@ function Navbar() {
                         display: 'flex',
                         flexDirection: { xs: 'row', md: 'column' },
                         width: '100%',
-                        mx: '1.5rem',
+                        mx: '1.75rem',
                     }}>
                     <Typography
                         component="h1"
@@ -61,7 +72,7 @@ function Navbar() {
                             fontFamily: 'cursive',
                             fontSize: '4rem',
                             textAlign: 'center',
-                            mb: { xs: '0', md: '1.5rem' },
+                            mb: { xs: '.5rem', md: '1.5rem' },
                             mx: 'auto',
                             zIndex: '1',
                         }}>
@@ -103,59 +114,69 @@ function Navbar() {
                             <ColorModeButton />
                         </Box>
                     </Box>
+
                     <Box
                         sx={{
                             flexGrow: 0,
+                            position: 'absolute',
+                            height: '100%',
+                            right: '1.75rem',
                             display: { xs: 'flex', md: 'none' },
+                            zIndex: '2',
                         }}>
                         <IconButton
                             size="large"
                             aria-label="navigation menu"
                             aria-controls="menu-navbar"
                             aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit">
+                            onClick={toggleDrawer(true)}
+                            color="inherit"
+                            sx={{
+                                alignSelf: 'center',
+                            }}>
                             <MenuIcon />
                         </IconButton>
-                        <Menu
-                            id="menu-navbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}>
-                            <MenuItem>
-                                <ColorModeButton />
-                            </MenuItem>
-                            {pages.map(page => (
-                                <MenuItem key={page.name}>
-                                    <Typography
-                                        variant="h6"
-                                        component="span"
-                                        textAlign="center">
-                                        <Link
+                    </Box>
+                    <Drawer
+                        id="menu-navbar"
+                        anchor={'right'}
+                        open={drawerState}
+                        onClose={toggleDrawer(false)}
+                        sx={{
+                            display: { xs: 'block', md: 'none' },
+                        }}>
+                        <Box
+                            sx={{ width: 250 }}
+                            role="presentation"
+                            onClick={toggleDrawer(false)}
+                            onKeyDown={toggleDrawer(false)}>
+                            <List>
+                                {pages.map(page => (
+                                    <ListItem key={page.name} disablePadding>
+                                        <ListItemButton
                                             component={RouterLink}
                                             to={page.link}
-                                            underline="hover"
-                                            color="inherit"
-                                            onClick={handleCloseNavMenu}>
-                                            {page.name}
-                                        </Link>
-                                    </Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
+                                            color="inherit">
+                                            <ListItemText primary={page.name} />
+                                        </ListItemButton>
+                                    </ListItem>
+                                ))}
+                            </List>
+                            <Divider />
+                            <List>
+                                <ListItem disablePadding>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            <ColorModeButton />
+                                        </ListItemIcon>
+                                        <ListItemText
+                                            primary={'Toggle color mode'}
+                                        />
+                                    </ListItemButton>
+                                </ListItem>
+                            </List>
+                        </Box>
+                    </Drawer>
                 </Box>
             </Toolbar>
         </AppBar>
