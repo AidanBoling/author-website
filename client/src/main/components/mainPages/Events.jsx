73213210cf2689d93eac72ea0612ee1ae7d456e9
Aidan Boling <aidanboling@gmail.com>
2023-12-04 18:58:28 -0800

    Update Events page content

--- client/src/main/components/mainPages/Events.jsx
+++ client/src/main/components/mainPages/Events.jsx
@@ -15,50 +15,94 @@ function Events({ children }) {
                         {paragraph}
                     </Typography>
                 ))}
-                {pageContent.headerText.bullets.map((paragraph, i) => (
+                <Box component="ul" p={0} mb={'2rem'}>
+                    {pageContent.headerText.bullets.map((paragraph, i) => (
+                        <Box
+                            key={i}
+                            component="li"
+                            sx={{
+                                m: 0,
+                                mb: '1rem',
+                                ml: { sxs: '4rem' },
+                                listStyle: 'none',
+                            }}>
+                            <Typography
+                                variant="h6"
+                                component="span"
+                                mb={0}
+                                fontStyle={'italic'}>
+                                {paragraph.title}
+                            </Typography>
+                            <Typography component="span">
+                                {paragraph.body}
+                            </Typography>
+                        </Box>
+                    ))}
+                </Box>
+                <Box my={'3rem'}>
                     <Box
-                        key={i}
-                        component="p"
-                        m={0}
-                        mb={'1rem'}
-                        ml={{ sxs: '4rem' }}>
+                        component="details"
+                        sx={{
+                            borderWidth: '1px',
+                            borderColor: 'primary.dark',
+                            borderStyle: 'solid',
+                            borderRadius: '.20rem',
+                        }}>
+                        <Typography
+                            component="summary"
+                            sx={{
+                                listStylePosition: 'outside',
+                                marginLeft: '1.6rem',
+                                marginY: '.8rem',
+                                paddingLeft: '8px',
+                            }}>
+                            See a list of {authorInfo.author.title}
+                            {authorInfo.author.lName}'s past speaking
+                            engagements
+                        </Typography>
+                        <Box component="ul" sx={{ ml: '1.5rem', my: '1rem' }}>
+                            {pageContent.headerText.speakingList.map(
+                                (item, i) => (
+                                    <Typography
+                                        key={i}
+                                        component="li"
+                                        sx={{
+                                            listStylePosition: 'outside',
+                                            marginLeft: '1rem',
+                                            mb: '.8rem',
+                                            paddingLeft: '.5rem',
+                                        }}>
+                                        {item}
+                                    </Typography>
+                                )
+                            )}
+                        </Box>
+                    </Box>
+                    <Box my={'1.5rem'}>
                         <Typography
                             variant="h6"
                             component="span"
-                            mb={0}
-                            fontStyle={'italic'}>
-                            {paragraph.title}
+                            sx={{ fontWeight: 480 }}>
+                            Interested in booking {authorInfo.author.title}
+                            {authorInfo.author.lName}?
                         </Typography>
-                        <Typography component="span">
-                            {paragraph.body}
+                        <Typography
+                            variant="h6"
+                            component="span"
+                            sx={{ fontWeight: 260 }}>
+                            <MuiLink
+                                component={Link}
+                                href={'/contact'}
+                                underline="none"
+                                sx={{
+                                    marginLeft: '1ch',
+                                    ':hover': { color: 'primary.dark' },
+                                }}>
+                                Contact her
+                            </MuiLink>{' '}
+                            for fees and pricing.
                         </Typography>
                     </Box>
-                ))}
-                <Box my={'3rem'}>
-                    <Typography
-                        variant="h6"
-                        component="span"
-                        sx={{ fontWeight: 480 }}>
-                        {/* <b> */}
-                        Interested in booking {authorInfo.author.title}
-                        {authorInfo.author.lName}? {/* </b> */}
-                    </Typography>
-                    <Typography
-                        variant="h6"
-                        component="span"
-                        sx={{ fontWeight: 260 }}>
-                        <MuiLink
-                            component={Link}
-                            href={'/contact'}
-                            underline="none"
-                            sx={{
-                                marginLeft: '1ch',
-                                ':hover': { color: 'primary.dark' },
-                            }}>
-                            Contact her
-                        </MuiLink>{' '}
-                        for fees and pricing.
-                    </Typography>
                 </Box>
             </Box>
             <Typography variant="h3" component="h3" mb="1.5rem" color="primary">
